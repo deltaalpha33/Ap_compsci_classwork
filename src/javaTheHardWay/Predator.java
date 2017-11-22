864cd66f0a48c167f85e24758c186ebea837fb98
deltaalpha33 <dalexakos@gmail.com>
2017-11-22 14:23:47 -0500

    prey class

--- src/javaTheHardWay/Predator.java
+++ src/javaTheHardWay/Predator.java
@@ -2,4 +2,8 @@ package javaTheHardWay;
 
 public class Predator extends Animal{
 
+	public Predator(Wilderness wilderness, String string, Trait trait, Trait trait2) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
